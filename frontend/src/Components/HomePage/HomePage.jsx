17d7a224cc7a7f78fa3b59bf5247355d4a170d9d
alokt <aloktripathy242@gmail.com>
2024-03-13 21:15:12 -0400

    HomePage left part

--- frontend/src/Components/HomePage/HomePage.jsx
+++ frontend/src/Components/HomePage/HomePage.jsx
@@ -1,12 +1,13 @@
 import { Grid } from '@mui/material'
 import React from 'react'
+import Navigation from '../Navigation/Navigation'
 
 const HomePage = () => {
   return (
     <Grid container xs={12} className='px-5 lg:px-36 justify-between'>
         
         <Grid item xs={0} lg={2.5} className='hidden lg:block w-full relative'>
-        <p className='text-center'>first part</p>
+          <Navigation/>
         </Grid>
         <Grid item xs={12} lg={6} className='hidden lg:block w-full relative'>
         <p className='text-center'>middle part</p>
